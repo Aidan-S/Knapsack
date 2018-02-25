2cc67a329cf3ff005015b7629457f16a07addb82
Aidan <Aidan@Aidan-PC>
2018-02-25 13:41:46 -0500

    part a working mostly, part b not so much

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Knapsack {
@@ -46,35 +47,54 @@ public class Knapsack {
 		return input;	
 	}
 	
-	/*
-	 * ----------------------------------------------------------------------------------------------------------
-	 * 
-	 * RECURSIVE PART YA DUMMY
-	 * 
-	 * 
-	 * 
-	 */
+
 	public static int knapsackSum(int[] w, int n, int limit) {
-		if(limit == 0 || n == 0) {
+		if(limit < 1 || n < 1) {
 			return 0;
 		}
 		
 		
 		if(w[n-1] > limit) {
 			return knapsackSum(w, n - 1, limit);
-		}
-			
-		int a = knapsackSum(w, n - 1, limit - w[n-1]);
-		int b = knapsackSum(w, n - 1, limit);
-		if(a>b) {
-			 return a;
 		}else {
-			return b;
+			
+		int a = knapsackSum(w, n - 1, limit - w[n-1])  +  w[n-1];
+		int b = knapsackSum(w, n - 1, limit)  +  w[n-1];
+		
+		if(a >= b) {
+			if(a <=limit) {
+				return a;
+			} 
+			
+		}else {
+			if(b <=limit) {
+				return b;
+			}
 		}
+		}
+		return 0;
+	}
+	
+	
+	
+	
+	public static int knapsackSum(int[] w, int n, int limit, List<Integer> list) {
+		if(limit < 1 || n < 1) {
+			return 0;
+		}
+		
+		if(w[n-1] > limit) {
+			return knapsackSum(w, n - 1, limit, list);
+		}
+		
+		ArrayList<Integer> box1 = (ArrayList<Integer>) list;
+		ArrayList<Integer> box2 = (ArrayList<Integer>) list;
+		
+		return 0;
+		
 		
 	}
 	
-	//-----------------------------------------------------------------------------------------------------------
 	
 	public static void addFiles(Scanner list) {
 		String line;
@@ -88,12 +108,42 @@ public class Knapsack {
 		int count = -1;
 		while (list.hasNextLine()) {
 		    count++;
-		    list.nextLine();
+		    if(list.hasNextLine()) {   
+		    	list.nextLine();
+		    }
 		}
 		return count;
 	}
 	
-
+	public static int spot(ArrayList<Integer> box) {
+		int k =0;
+		while(box.get(k) != null) {
+			k++;
+		}
+		return k;
+	}
+	
+	public static String runFile(Scanner list, int f, int c, List<Integer> box) {
+		String limit = list.nextLine();
+		String line = list.nextLine();
+		int[] melons = new int[c];
+		String nums = limit + "    ";
+		for(int i = 0; i < c; i++) {
+			melons[i] = Integer.parseInt(line);
+			if(list.hasNextLine()) {
+				line = list.nextLine();
+			}
+		}
+		for(int j = 0; j < melons.length; j++) {
+			nums += melons[j] + ", ";
+		}
+		nums = nums.substring(0, nums.length() - 2);
+		
+		int total = knapsackSum(melons, c, Integer.parseInt(limit), box);
+		
+		return (files.get(f) + "   " + nums + "   " + total);
+		
+	}
 	
 	public static String runFile(Scanner list, int f, int c) {
 		String limit = list.nextLine();
@@ -118,13 +168,13 @@ public class Knapsack {
 	}
 	
 	
-	
 	public static void main(String[] args) {
 		PrintWriter out = outputFile("knapsack.txt");
 		Scanner file = openWords(args[0], 1, out);
 		Scanner keyboard = new Scanner(System.in);
 		addFiles(file);
 		int k;
+		ArrayList<Integer> list;
 		
 		if(files.size() < 1) {
 			System.out.println("Enter a file name: ");
@@ -132,26 +182,18 @@ public class Knapsack {
 		}
 		
 		for(int i = 0; i < files.size(); i++) {	
+			if(size(file)>=0) {
 				file = openWords(files.get(i), (i + 1), out);			
 				k = (size(file));
 				file = openWords(files.get(i), (i + 1), out);
+				list = new ArrayList<Integer>();
 				out.println(runFile(file, i, k) + "\n");
-		}
 		
+			}
+		}
 		
 		file.close();
 		out.close();
 		
 	}	
 }
-
-
-
-
-
-
-
-
-
-
-
