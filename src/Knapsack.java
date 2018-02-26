5489459a354d93fc7c0f19bb704627eaf82c3a8d
Aidan <Aidan@Aidan-PC>
2018-02-25 20:04:56 -0500

    progress?

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -11,9 +11,17 @@ public class Knapsack {
 	
 	
 	/**
-	 * 
-	 * @param fName
-	 * @return
+
+	 * @author Aidan-S
+
+	 * date: February 25th, 2018
+
+	 * method: Create a print writer so that I can print out my output
+
+	 * @param fName: name of the file I want to print to
+
+	 * @return: A Print Writer that I can print to
+
 	 */
 	public static PrintWriter outputFile(String fName) {
 			File fileName = new File(fName);
@@ -21,7 +29,7 @@ public class Knapsack {
 			try {
 				output = new PrintWriter(fileName);
 			} catch (FileNotFoundException ex) {
-				System.out.print("Cannot open " + fName + " , it may not exist");
+				System.out.print("Cannot open " + fName + ", it may not exist");
 				return null;
 			}
 			return output;
@@ -29,11 +37,19 @@ public class Knapsack {
 	
 	
 	/**
-	 * 
-	 * @param fname
-	 * @param fnum
-	 * @param out
-	 * @return
+
+	 * @author Aidan-S
+
+	 * date: February 25th, 2018
+
+	 * method: Create a Scanner so that I can read in the given files
+
+	 * @param fname: name of the file I want to read
+	 * @param fnum: the number of the file I'm opening
+	 * @param out: printwriter that I'm printing to
+
+	 * @return: A Scanner of the file I send in
+
 	 */
 	public static Scanner openWords(String fname, int fnum, PrintWriter out) {
 		File file = new File(fname);
@@ -41,13 +57,27 @@ public class Knapsack {
 		try {
 			input = new Scanner(file);
 		} catch (FileNotFoundException ex) {
-			out.println("Part " + fnum + ": Unable to Open File");
+			System.out.print("Cannot open " + fname + ", it may not exist");
 			return null;
 		}
 		return input;	
 	}
 	
+	/**
 
+	 * @author Aidan-S
+
+	 * date: February 25th, 2018
+
+	 * method: recursively goes through each of the items in the list(w[n]), testing whether we would get a better value from that item being there or not while staying within the limit
+
+	 * @param limit: how much room is left in the bag
+	 * @param n: what number item we're on
+	 * @param w: array of weights
+
+	 * @return: total that we can fit in without overflow
+
+	 */
 	public static int knapsackSum(int[] w, int n, int limit) {
 		if(limit < 1 || n < 1) {
 			return 0;
@@ -58,27 +88,44 @@ public class Knapsack {
 			return knapsackSum(w, n - 1, limit);
 		}else {
 			
-		int a = knapsackSum(w, n - 1, limit - w[n-1])  +  w[n-1];
-		int b = knapsackSum(w, n - 1, limit)  +  w[n-1];
+			int a = knapsackSum(w, n - 1, limit - w[n-1])  +  w[n-1];
+			int b = knapsackSum(w, n - 1, limit)  +  w[n-1];
 		
-		if(a >= b) {
-			if(a <=limit) {
-				return a;
-			} 
+			if(a >= b) {
+				if(a <=limit) {
+					return a;
+				} 
 			
-		}else {
-			if(b <=limit) {
+			}else {
+				if(b <=limit) {
 				return b;
+				}
 			}
-		}
 		}
 		return 0;
 	}
+
 	
 	
+	/**-------------------------------------------------------------------------------------------------------
+
+	 * @author Aidan-S
+
+	 * date: February 25th, 2018
+
+	 * method: recursively goes through each of the items in the list(w[n]), testing whether we would get a better value from that item being there or not while staying within the limit  and then keeping track of the watermelons used
+
+	 * @param limit: how much room is left in the bag
+	 * @param n: what number item we're on
+	 * @param w: array of weights
+	 * @param list: array list to hold what items are used
+
+	 * @return: total that we can fit in without overflow
+
+		-------------------------------------------------------------------------------------------------------
+	 */
 	
-	
-	public static int knapsackSum(int[] w, int n, int limit, List<Integer> list) {
+	 public static int knapsackSum(int[] w, int n, int limit, List<Integer> list) {
 		if(limit < 1 || n < 1) {
 			return 0;
 		}
@@ -90,12 +137,26 @@ public class Knapsack {
 		ArrayList<Integer> box1 = (ArrayList<Integer>) list;
 		ArrayList<Integer> box2 = (ArrayList<Integer>) list;
 		
+		
+		
 		return 0;
 		
 		
 	}
 	
-	
+	/**
+
+	 * @author Aidan-S
+
+	 * date: February 25th, 2018
+
+	 * method: put all files I need to evaluate on files list
+
+	 * @param list: list of files to add
+
+	 * return: none
+
+	 */
 	public static void addFiles(Scanner list) {
 		String line;
 		while(list.hasNextLine()){
@@ -104,6 +165,19 @@ public class Knapsack {
 		}
 	}
 	
+	/**
+
+	 * @author Aidan-S
+
+	 * date: February 25th, 2018
+
+	 * method: find the number of watermelons that I can add to the sack
+
+	 * @param list: scanner of the file with the watermelon information
+
+	 * @return: none
+
+	 */
 	public static int size(Scanner list) {
 		int count = -1;
 		while (list.hasNextLine()) {
@@ -115,6 +189,19 @@ public class Knapsack {
 		return count;
 	}
 	
+	/**
+
+	 * @author Aidan-S
+
+	 * date: February 25th, 2018
+
+	 * method: find the nearest open slot in array list
+
+	 * @param box: array list to be evaluated
+
+	 * @return: int where the next spot is
+
+	 */
 	public static int spot(ArrayList<Integer> box) {
 		int k =0;
 		while(box.get(k) != null) {
@@ -123,6 +210,22 @@ public class Knapsack {
 		return k;
 	}
 	
+	/**
+
+	 * @author Aidan-S
+
+	 * date: February 25th, 2018
+
+	 * method: organize all my variables so that I can call my recursive method with a list
+
+	 * @param  list: file with the watermelon information 
+	 * @param  f: what number file in files array list im looking at
+	 * @param  c: how many watermelons
+	 * @param  box: array list of what watermelons will be used (to be filled later)
+	 * 
+	 * @return: String of what the best solution is and the watermelon information
+
+	 */
 	public static String runFile(Scanner list, int f, int c, List<Integer> box) {
 		String limit = list.nextLine();
 		String line = list.nextLine();
@@ -141,10 +244,25 @@ public class Knapsack {
 		
 		int total = knapsackSum(melons, c, Integer.parseInt(limit), box);
 		
-		return (files.get(f) + "   " + nums + "   " + total);
+		return (files.get(f) + "   " + nums + "\n\n" + total);
 		
 	}
 	
+	/**
+
+	 * @author Aidan-S
+
+	 * date: February 25th, 2018
+
+	 * method: organize all my variables so that I can call my recursive method without a list
+
+	 * @param  list: file with the watermelon information 
+	 * @param  f: what number file in files array list im looking at
+	 * @param  c: how many watermelons         
+	  
+	 * @return: String of what the best solution is and the watermelon information
+
+	 */
 	public static String runFile(Scanner list, int f, int c) {
 		String limit = list.nextLine();
 		String line = list.nextLine();
@@ -163,11 +281,23 @@ public class Knapsack {
 		
 		int total = knapsackSum(melons, c, Integer.parseInt(limit));
 		
-		return (files.get(f) + "   " + nums + "   " + total);
+		return (files.get(f) + "   " + nums + "\n\n" + total);
 		
 	}
 	
-	
+	/**
+
+	 * @author Aidan-S
+
+	 * date: February 25th, 2018
+
+	 * method: Run the code, take in the file to be turned into all the different files that need to be opened, calling the methods that lead to the recursive methods
+
+	 * @param  args: arguments
+
+	 * return: none
+
+	 */
 	public static void main(String[] args) {
 		PrintWriter out = outputFile("knapsack.txt");
 		Scanner file = openWords(args[0], 1, out);
@@ -178,21 +308,26 @@ public class Knapsack {
 		
 		if(files.size() < 1) {
 			System.out.println("Enter a file name: ");
-			files.set(0, keyboard.nextLine());
+			files.add(keyboard.nextLine());
+			file = openWords(files.get(0), 1, out);
 		}
 		
 		for(int i = 0; i < files.size(); i++) {	
-			if(size(file)>=0) {
+			if(file != null) {
 				file = openWords(files.get(i), (i + 1), out);			
 				k = (size(file));
 				file = openWords(files.get(i), (i + 1), out);
 				list = new ArrayList<Integer>();
-				out.println(runFile(file, i, k) + "\n");
-		
+				out.println(runFile(file, i, k, list) + "\n");
+				for(int p = 0; p < list.size(); p++) {
+					out.println(list.get(p) + "\n");
+				}
 			}
 		}
 		
-		file.close();
+		
+		if(file != null) {file.close();}
+		
 		out.close();
 		
 	}	
